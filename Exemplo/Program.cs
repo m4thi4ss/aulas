c5a8310effc73f9b76c9bbf10c8959e5f036c494
FIC <fic@info.local>
2025-03-18 21:56:25 -0300

    Aula

---
+++ Exemplo/Program.cs
@@ -0,0 +1,61 @@
+﻿// ESTRUTURAS CONDICIONAIS 
+
+// ESTRUTURAS DE REPETICAO
+// REPETE UMA ACAO 
+
+// Imprimir todos os numeros de 1 a 5 
+// Enquanto - While 
+
+int numero = 1;
+
+// Repete uma acao, ENQUANTO algo for verdade
+while (numero < 6)
+{
+    // Se colocar somente o Console.WriteLine(numero) - ele vai imprimir o numero 1 infinitamente.
+    Console.WriteLine(numero);
+    //Incremento
+    //numero = numero + 1 (O atalho ele faz a mesma coisa);
+    numero++;
+}
+// Faca - Enquanto (Do/While)
+//Console.WriteLine("Informe a idade: ");
+//int idade = int.Parse(Console.ReadLine());
+
+//while (idade != 22)
+//{
+//    Console.WriteLine("Informe a idade: ");
+//    idade = int.Parse(Console.ReadLine());
+//}
+
+int idade;
+do
+{
+    Console.WriteLine("Informe a idade: ");
+    idade = int.Parse(Console.ReadLine());
+} while (idade != 22);
+
+// Para - FOR
+//Quero imprimir os numeros do 1 ao 5 
+
+//1 - Cria uma variaval chamadda i ("indice")
+//2-
+//3 - Incremento ao executar as chaves 
+for (int i = 0; i < 6;i++) //i++ e o incremento igual o numero ++ (Caso queira pular por outro numero ao inves de 1 so usar essa conta i = i + 2)
+{
+    
+}
+
+// Tratamento de Execucao 
+// Expection (Conjuntos de erros)
+// IOException - in and out (entrada e saida)
+//OverFloeException - Ele e usado quando o usuario coloca um numero muito grande que acaba transbordando 
+//formatException - Erros no formato (tipo de dados)
+
+//try/catch - Tentar/Pegar 
+//Breakpoint
+
+try
+{
+    Console.WriteLine(
+
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,66 @@
+﻿//Crie um programa que exiba os números de 1 a 20 na tela.
+
+//int numero = 1;
+
+//while (numero <= 20)
+//{
+//    Console.WriteLine(numero);
+//    numero++;
+//}
+
+//Peça ao usuário para inserir a senha "1234". Continue pedindo até que ele acerte.
+
+//int senha;
+
+//do
+//{
+//    Console.WriteLine("Informe a senha: ");
+//    senha = int.Parse(Console.ReadLine());
+//} while (senha != 1234);
+
+//Peça ao usuário um número e exiba a tabuada dele de 1 a 10.
+
+//Console.WriteLine("Informe um numero: ");
+//int tab = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < 11; i++)
+//{
+//    Console.WriteLine(i * tab);
+//}
+
+//resposta do professor 
+//Console.WriteLine("Informe um valor:");
+//int valor = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < 11; i++)
+//{
+//    Console.WriteLine($"{valor} * {valor * i}");
+//}
+//Peça um número ao usuário e faça uma contagem regressiva até 0.
+
+//Console.WriteLine("Informe um numero:");
+//int reg = int.Parse(Console.ReadLine());    
+
+//for (int i = reg; i > 0; i--)
+//{
+//    Console.WriteLine(i);
+//    reg ++;
+//}
+
+//Solicite notas ao usuário (entre 0 e 10) até que ele insira um número inválido. Depois, exiba a média das notas.
+
+int nota;
+int nota02 = 1;
+
+
+do
+{
+    Console.WriteLine("informe um numero: ");
+    nota = int.Parse(Console.ReadLine());
+} while (nota > 0 && nota < 11);
+
+
+
+
+
+
