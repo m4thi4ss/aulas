8a37edef38706b3e5b6442ce0da719ceaf3c2abc
FIC <fic@info.local>
2025-03-21 20:49:02 -0300

    Projeto atualizado

--- Exemplo/obj/Debug/net9.0/Exemplo.AssemblyInfo.cs
+++ Exemplo/obj/Debug/net9.0/Exemplo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exemplo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7172a86ebabc0b7fc17b4fb4c9b2d548abd9911")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exemplo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exemplo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PROJETO C#/Program.cs
+++ PROJETO C#/Program.cs
@@ -1,32 +1,62 @@
 ﻿//Inserir as notas de um aluno. (Cada aluno tem 5 notas)
 
 int[] NotaAluno = new int[5];
-int SomaNotas = 0; 
+double SomaNotas = 0;
 int Qtdenotas = 0;
+int Lugar = 1;
+//int Escolha ;
 
-for (int i = 0; i < 5; i++)
+//Console.WriteLine("=== Gerenciador de Notas ===");
+
+//Console.WriteLine("1. Cadastrar notas");
+//Console.WriteLine("2. Listar notas");
+//Console.WriteLine("3. Estatiscas");
+//Console.WriteLine("4. Sair");
+
+
+//    Console.WriteLine("Escolha uma opção:");
+//    Escolha[i] = int.Parse(Console.ReadLine());
+//    if(Escolha[i] == 0)
+
+
+
+
+try
 {
-   
-        Console.WriteLine("Me inform a nota:");
+    for (int i = 0; i < 5; i++)
+    {
+
+        Console.WriteLine("Digite a " + Lugar + "ª nota:");
         NotaAluno[i] = int.Parse(Console.ReadLine());
         SomaNotas = NotaAluno[i] + SomaNotas;
         Qtdenotas++;
-    do
+        Lugar++;
+        if (NotaAluno[i] < 0 && NotaAluno[i] > 10)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+    foreach (int i in NotaAluno)
     {
-        Console.WriteLine(NotaAluno[i]);
-    } while (NotaAluno[i] > 0 && NotaAluno[i] < 10);
+        Console.WriteLine(i);
+    }
+    double media = SomaNotas / Qtdenotas;
+    Console.WriteLine("E a média seria:" + media);
+    if (media < 7)
+    {
+        Console.WriteLine("O aluno foi REPROVADO!");
+    }
+    else if (media >= 7)
+    {
+        Console.WriteLine("O aluno foi APROVADO!");
+    }
 }
-foreach (int i in NotaAluno)
+catch (ArgumentOutOfRangeException ex)
 {
-    Console.WriteLine(i);
+    Console.WriteLine("Erro: Não pode inserir numero negativos ou acima de 10!");
 }
-int media = SomaNotas / Qtdenotas;
-Console.WriteLine("E a media seria:" + media);
-if (media < 7)
+catch (FormatException)
 {
-    Console.WriteLine("Reprovado gato(a)");
+    Console.WriteLine("Erro: Não pode inserir letras!");
 }
-else if (media >= 7)
-{
-    Console.WriteLine("Voce e um meninao esperto, PASSOU!!!");
-}
+
